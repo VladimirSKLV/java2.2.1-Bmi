0f4bf15a8fd7dee29ed09f27fb59ad867e33e22b
VladimirSKLV <vladimirsokolov969@gmail.com>
2022-07-12 21:17:27 +0300

    v1.1

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,6 @@
 public class BmiService {
-    public double calculate(double weight, double height){
-        if(weight < 0 || height < 0){
-            weight =0;
-        }
-        return weight/(height*height);
+    public double calculate(double weight, double height) {
+        if (weight < 0 || height < 0) weight = 0;
+        return weight / (height * height);
     }
 }
